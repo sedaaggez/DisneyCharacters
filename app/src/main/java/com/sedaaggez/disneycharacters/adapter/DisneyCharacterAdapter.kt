7e9b566d68bb680bd564e7a775c242c60e22152e
sedaaggez <seda-aggez@hotmail.com>
2021-05-15 11:00:23 +0300

    create adapter (DisneyCharacterAdapter)

---
+++ app/src/main/java/com/sedaaggez/disneycharacters/adapter/DisneyCharacterAdapter.kt
@@ -0,0 +1,36 @@
+package com.sedaaggez.disneycharacters.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.sedaaggez.disneycharacters.R
+import com.sedaaggez.disneycharacters.model.Character
+import kotlinx.android.synthetic.main.item_disney_character.view.*
+
+class DisneyCharacterAdapter(val characterList: ArrayList<Character>): RecyclerView.Adapter<DisneyCharacterAdapter.DisneyCharacterViewHolder>()  {
+
+    class DisneyCharacterViewHolder(var view: View) : RecyclerView.ViewHolder(view) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DisneyCharacterViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val view = inflater.inflate(R.layout.item_disney_character, parent, false)
+        return DisneyCharacterViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: DisneyCharacterViewHolder, position: Int) {
+        holder.view.textViewCharacter.text = characterList[position].name
+    }
+
+    override fun getItemCount(): Int {
+        return characterList.size
+    }
+
+    fun updateCharacterList(newCharacterList: List<Character>) {
+        characterList.clear()
+        characterList.addAll(newCharacterList)
+        notifyDataSetChanged()
+    }
+}
