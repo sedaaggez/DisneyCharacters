f29540be155ffd2eeac62a29c421b569e34ed052
sedaaggez <seda-aggez@hotmail.com>
2021-06-06 21:58:43 +0300

    update model for model

--- app/src/main/java/com/sedaaggez/disneycharacters/model/CharacterList.kt
+++ app/src/main/java/com/sedaaggez/disneycharacters/model/CharacterList.kt
@@ -1,8 +1,16 @@
 package com.sedaaggez.disneycharacters.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+
+@Entity
 data class CharacterList(
+    @ColumnInfo(name = "count")
     val count: Integer?,
+    @ColumnInfo(name = "previousPage")
     val previousPage: String?,
+    @ColumnInfo(name = "nextPage")
     val nextPage: String?,
+    @ColumnInfo(name = "data")
     val data: List<Character>?
 )
