67f858d1fcf40ff55afb00f80f3fd216401a6a32
sedaaggez <seda-aggez@hotmail.com>
2021-06-25 12:52:14 +0300

    fragment data binding

--- app/src/main/java/com/sedaaggez/disneycharacters/view/DisneyCharacterDetailFragment.kt
+++ app/src/main/java/com/sedaaggez/disneycharacters/view/DisneyCharacterDetailFragment.kt
@@ -5,17 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.sedaaggez.disneycharacters.R
-import com.sedaaggez.disneycharacters.util.downloadFromUrl
-import com.sedaaggez.disneycharacters.util.placeholderProgressBar
+import com.sedaaggez.disneycharacters.databinding.FragmentDisneyCharacterDetailBinding
 import com.sedaaggez.disneycharacters.viewmodel.DisneyCharacterDetailViewModel
-import kotlinx.android.synthetic.main.fragment_disney_character_detail.*
 
 class DisneyCharacterDetailFragment : Fragment() {
+
     private lateinit var viewModel : DisneyCharacterDetailViewModel
     private var characterUuid = 0
+    private lateinit var dataBinding: FragmentDisneyCharacterDetailBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,8 +27,13 @@ class DisneyCharacterDetailFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_disney_character_detail, container, false)
+        dataBinding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_disney_character_detail,
+            container,
+            false
+        )
+        return dataBinding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -47,10 +53,8 @@ class DisneyCharacterDetailFragment : Fragment() {
     private fun observeLiveData() {
         viewModel.characterLiveData.observe(viewLifecycleOwner, Observer { character ->
             character?.let {
-                textViewDetailCharacterName.text = character.name
-                context?.let {
-                    imageViewDetailCharacter.downloadFromUrl(character.imageUrl, placeholderProgressBar(it))
-                }
+                dataBinding.character = character
+                // TODO: recycler view data binding
             }
         })
     }
