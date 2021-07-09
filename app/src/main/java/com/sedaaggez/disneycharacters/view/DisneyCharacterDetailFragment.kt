616634a2300f33e41ade4d5be965cec7f6d0d43d
sedaaggez <seda-aggez@hotmail.com>
2021-07-09 18:00:40 +0300

    updateTvShowList

--- app/src/main/java/com/sedaaggez/disneycharacters/view/DisneyCharacterDetailFragment.kt
+++ app/src/main/java/com/sedaaggez/disneycharacters/view/DisneyCharacterDetailFragment.kt
@@ -1,11 +1,11 @@
 package com.sedaaggez.disneycharacters.view
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -21,11 +21,6 @@ class DisneyCharacterDetailFragment : Fragment() {
     private var characterUuid = 0
     private lateinit var dataBinding: FragmentDisneyCharacterDetailBinding
     private val disneyTvShowAdapter = DisneyTvShowAdapter(arrayListOf())
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -56,14 +51,15 @@ class DisneyCharacterDetailFragment : Fragment() {
         observeLiveData()
     }
 
-
     private fun observeLiveData() {
         viewModel.characterLiveData.observe(viewLifecycleOwner, Observer { character ->
             character?.let {
                 dataBinding.character = character
-                // TODO: recycler view data binding
+                if (character.tvShows!![0] != "") {
+                    disneyTvShowAdapter.updateTvShowList(character.tvShows!!)
+                    textViewTvShows.visibility = View.VISIBLE
+                }
             }
         })
     }
-
 }
