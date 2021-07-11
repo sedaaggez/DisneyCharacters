7f7c21ebe0e403b202d9e4556eedd4d37bf5d540
sedaaggez <seda-aggez@hotmail.com>
2021-07-11 14:37:44 +0300

    binding tv show adapter

--- app/src/main/java/com/sedaaggez/disneycharacters/adapter/DisneyTvShowAdapter.kt
+++ app/src/main/java/com/sedaaggez/disneycharacters/adapter/DisneyTvShowAdapter.kt
@@ -1,26 +1,27 @@
 package com.sedaaggez.disneycharacters.adapter
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.sedaaggez.disneycharacters.R
-import kotlinx.android.synthetic.main.item_disney_tv_show.view.*
+import com.sedaaggez.disneycharacters.databinding.ItemDisneyTvShowBinding
 
-class DisneyTvShowAdapter(val tvShowList: ArrayList<String>): RecyclerView.Adapter<DisneyTvShowAdapter.DisneyTvShowViewHolder>()  {
+class DisneyTvShowAdapter(val tvShowList: ArrayList<String>) :
+    RecyclerView.Adapter<DisneyTvShowAdapter.DisneyTvShowViewHolder>() {
 
-    class DisneyTvShowViewHolder(var view: View) : RecyclerView.ViewHolder(view) {
+    class DisneyTvShowViewHolder(var view: ItemDisneyTvShowBinding) : RecyclerView.ViewHolder(view.root) {
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DisneyTvShowViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        val view = inflater.inflate(R.layout.item_disney_tv_show, parent, false)
+        val view = DataBindingUtil.inflate<ItemDisneyTvShowBinding>(inflater, R.layout.item_disney_tv_show, parent, false)
         return DisneyTvShowViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: DisneyTvShowViewHolder, position: Int) {
-        holder.view.textViewTvShowName.text = tvShowList[position]
+        holder.view.tvShow = tvShowList[position]
     }
 
     override fun getItemCount(): Int {
