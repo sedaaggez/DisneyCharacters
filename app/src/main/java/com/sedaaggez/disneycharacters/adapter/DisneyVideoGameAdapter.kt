0467783270cb2ce2d541f7e2f06cb398def675be
sedaaggez <seda-aggez@hotmail.com>
2021-07-12 11:55:38 +0300

    binding video game adapter

--- app/src/main/java/com/sedaaggez/disneycharacters/adapter/DisneyVideoGameAdapter.kt
+++ app/src/main/java/com/sedaaggez/disneycharacters/adapter/DisneyVideoGameAdapter.kt
@@ -1,26 +1,26 @@
 package com.sedaaggez.disneycharacters.adapter
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.sedaaggez.disneycharacters.R
-import kotlinx.android.synthetic.main.item_disney_video_game.view.*
+import com.sedaaggez.disneycharacters.databinding.ItemDisneyVideoGameBinding
 
 class DisneyVideoGameAdapter(val videoGameList: ArrayList<String>): RecyclerView.Adapter<DisneyVideoGameAdapter.DisneyVideoGameHolder>()  {
 
-    class DisneyVideoGameHolder(var view: View) : RecyclerView.ViewHolder(view) {
+    class DisneyVideoGameHolder(var view: ItemDisneyVideoGameBinding) : RecyclerView.ViewHolder(view.root) {
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DisneyVideoGameHolder {
         val inflater = LayoutInflater.from(parent.context)
-        val view = inflater.inflate(R.layout.item_disney_video_game, parent, false)
+        val view = DataBindingUtil.inflate<ItemDisneyVideoGameBinding>(inflater, R.layout.item_disney_video_game, parent, false)
         return DisneyVideoGameHolder(view)
     }
 
     override fun onBindViewHolder(holder: DisneyVideoGameHolder, position: Int) {
-        holder.view.textViewVideoGameName.text = videoGameList[position]
+        holder.view.videoGame = videoGameList[position]
     }
 
     override fun getItemCount(): Int {
